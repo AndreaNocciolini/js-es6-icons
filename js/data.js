7b524d327a0af3280a44e2246822f767f870bac6
AndreaNocciolini <andreanocciolini93@gmail.com>
2021-12-13 17:37:08 +0100

    add comment bonus js

--- js/data.js
+++ js/data.js
@@ -215,3 +215,7 @@ filter.addEventListener(`change`, function(){
 			break;
 	}
 })
+
+// BONUS
+// 1- modificare la struttura dati fornita e valorizzare la proprietà "color" in modo dinamico: generare in modo casuale un codice colore, sapendo che la notazione esadecimale è formata dal simbolo "#" seguito da 6 caratteri alfanumerici compresi tra 0 e 9 e A e F.
+// 2- popolare le options della select della milestone 3 dinamicamente.
