4efc3bd2545b6a2b3e030c99e519b17636bc1564
AndreaNocciolini <andreanocciolini93@gmail.com>
2021-12-13 17:35:27 +0100

    milestone 3

--- js/data.js
+++ js/data.js
@@ -130,11 +130,88 @@ for (let i = 0; i < icons.length; i++) {
 	</div>
 	`
 	container.innerHTML += templateDiv
-
 }
-
-
-
 
 // Milestone 3
 // Aggiungere alla pagina una select in cui le options corrispondono ai vari tipi di icone (animal, vegetable, user). Quando l'utente seleziona un tipo dalla select, visualizzare solamente le icone corrispondenti.
+const filter = document.getElementById(`filter`);
+console.log(filter)
+
+// Creo array vuoti dove mettere gli elementi che mi interessano divisi per categorie (animals, vegetables, users)
+const animals = [];
+const vegetables = [];
+const users = [];
+
+// Riempio gli array vuoti appena creati
+icons.forEach(icon => {
+	switch (icon.type) {
+		case 'animal':
+			animals.push(icon);
+			break;
+		case 'vegetable':
+			vegetables.push(icon);
+			break;
+		case 'user':
+			users.push(icon);
+			break;
+	}
+});
+
+console.log(animals);
+console.log(users);
+console.log(vegetables);
+
+// Attraverso un addEventListener faccio in modo che, a seconda della selezione, venga stampata in pagina l'intera lista di icone o solo una parte (stampo solo uno degli array precedentemente creati)
+filter.addEventListener(`change`, function(){
+	console.log(filter.value)
+	switch (filter.value) {
+		case `all`:
+			container.innerHTML = ``
+			icons.forEach(element => {
+				container.innerHTML += `
+			<div class="icon-container">
+				<div class="icon"><i class="${element.family} ${element.prefix}${element.name}" style="color: ${element.color}"></i></div>
+				<div class="icon-title">${element.name}</div>
+			</div>
+			`
+			})
+			break;
+		case `animals`:
+			container.innerHTML = ``
+			animals.forEach(element => {
+				container.innerHTML += `
+			<div class="icon-container">
+				<div class="icon"><i class="${element.family} ${element.prefix}${element.name}" style="color: ${element.color}"></i></div>
+				<div class="icon-title">${element.name}</div>
+			</div>
+			`
+			})
+			break;
+		case `vegetables`:
+			container.innerHTML = ``
+			vegetables.forEach(element => {
+				container.innerHTML += `
+			<div class="icon-container">
+				<div class="icon"><i class="${element.family} ${element.prefix}${element.name}" style="color: ${element.color}"></i></div>
+				<div class="icon-title">${element.name}</div>
+			</div>
+			`
+			})
+			break;
+		case `users`:
+			container.innerHTML = ``
+			users.forEach(element => {
+				container.innerHTML += `
+			<div class="icon-container">
+				<div class="icon"><i class="${element.family} ${element.prefix}${element.name}" style="color: ${element.color}"></i></div>
+				<div class="icon-title">${element.name}</div>
+			</div>
+			`
+			})
+			break;
+		
+	
+		default:
+			break;
+	}
+})
